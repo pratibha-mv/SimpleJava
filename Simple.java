7bea4250f7f7c9109853a7846a96d2c0438fd3be
pratibha-mv <75791511+pratibha-mv@users.noreply.github.com>
2021-01-05 16:39:19 +0000

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -2,6 +2,7 @@ class Simple{
     public static void main(String args[]){  
      System.out.println("Hello Java"); 
         System.out.println("testing successful");
+           System.out.println("Have a good Day");
         
     }  
 }  
